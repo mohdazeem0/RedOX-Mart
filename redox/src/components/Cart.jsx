8bbd7f0a4dcddd7c18334d8abedfff5055cc8b9e
mohdazeem0 <mohdazm007@gmail.com>
2021-08-28 14:03:50 +0530

    updated

--- redox/src/components/Cart.jsx
+++ redox/src/components/Cart.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const Cart = () => {
   return (
     <div>
-      <h1>Its Cart </h1>
+      <h1>Its Junaids Cart </h1>
+      <h2>Its Junaids Cart </h2>
+      <h3>Its Junaids Cart </h3>
+      <h4>Its Junaids Cart </h4>
+      <h5>Its Junaids Cart </h5>
+      <h6>Its Junaids Cart </h6>
+      <p style={{ fontSize: "20px", color: "#" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+        voluptate id sit facere fugit ea vitae necessitatibus blanditiis facilis
+        alias.
+      </p>
     </div>
   );
 };
