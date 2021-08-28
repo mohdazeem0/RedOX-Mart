f7d9222b29b315a8a960fc9cbdab6c9c9209e69b
mohdazeem0 <mohdazm007@gmail.com>
2021-08-28 15:19:28 +0530

    updated redux

--- redox/src/components/Redux/Constants/actionTypes.js
+++ redox/src/components/Redux/Constants/actionTypes.js
@@ -1,5 +1,5 @@
-export const ActionType={
-    SET_PRODUCTS = "SET_PRODUCTS",
-    SELECTED_PRODUCT ="SELECTED_PRODUCT",  //to fetch details of selected products 
-    REMOVE_SELECTED_PRODUCT ="REMOVE_SELECTED_PRODUCT",  //to remove selected products
+export const ActionType = {
+  SET_PRODUCTS: "SET_PRODUCTS",
+  SELECTED_PRODUCT: "SELECTED_PRODUCT", //to fetch details of selected products
+  REMOVE_SELECTED_PRODUCT: "REMOVE_SELECTED_PRODUCT", //to remove selected products
 };
